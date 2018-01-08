c2cc4f72a0539eb9466d85e4be155d43fe5d2bcc
Hamza Ansari <hamzaansari209@gmail.com>
2018-01-08 11:11:26 +0530

    Add Carthage support

---
+++ Carthage/Build/iOS/HACameraView.framework/Headers/HACameraView.h
@@ -0,0 +1,19 @@
+//
+//  HACameraView.h
+//  HACameraView
+//
+//  Created by Hamza Ansari on 1/6/18.
+//  Copyright © 2018 Ansaris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HACameraView.
+FOUNDATION_EXPORT double HACameraViewVersionNumber;
+
+//! Project version string for HACameraView.
+FOUNDATION_EXPORT const unsigned char HACameraViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HACameraView/PublicHeader.h>
+
+
